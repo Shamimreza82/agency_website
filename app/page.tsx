c34696407873a4e199391d3889d3ce0eee8a6ecc
almamuntmn <almamuncool88@gmail.com>
2025-11-06 16:02:44 +0600

    cooming soon

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import ComingSoon from "@/components/ComingSoon";
 import Image from "next/image";
 
 export default function Home() {
   return (
-<div>
-<h1>Hi I am cool Man</h1>
-<h2>Hey i am cool  friend</h2>
-</div>
+    <div>
+      <ComingSoon />
+    </div>
   );
 }
