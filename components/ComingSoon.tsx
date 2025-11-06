260204d90cbe156565afa466f4e842d346f47e7a
almamuntmn <almamuncool88@gmail.com>
2025-11-06 16:04:28 +0600

    coming soon comp

--- components/ComingSoon.tsx
+++ components/ComingSoon.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ComingSoon = () => {
     return (
         <div>
-            <h1>Coming Soon</h1>
+            <h1 className='text-5xl text-center'>Coming Soon</h1>
         </div>
     );
 };
