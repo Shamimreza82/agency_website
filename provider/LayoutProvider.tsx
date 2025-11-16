cfb310699cf43faaf59e3a6dc3eea692e8142e5a
Shamimreza82 <shamimrezaone@gmail.com>
2025-11-16 21:34:39 +0600

    update login and sign button ready

--- provider/LayoutProvider.tsx
+++ provider/LayoutProvider.tsx
@@ -1,8 +1,10 @@
+import Navber from '@/components/layout/Navber'
 import { ReactNode } from 'react'
 
 const LayoutProvider = ({ children }: { children: ReactNode }) => {
     return (
         <div>
+            <Navber />
             {children}
         </div>
     )
