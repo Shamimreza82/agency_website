a57fb89fba3cdf24c4ca924de45f784883543596
Shamimreza82 <shamimrezaone@gmail.com>
2025-11-16 19:10:26 +0600

    updated

--- components/ComingSoon.tsx
+++ components/ComingSoon.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+
 
 const ComingSoon = () => {
     return (
         <div>
-            <h1 className='text-5xl text-center'>Coming Soon</h1>
+            <h1 className='text-5xl text-center h-screen flex items-center justify-center'>Coming Soon</h1>
         </div>
     );
 };
