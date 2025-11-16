ce578c8828593cba225d5ded6817646e6fcfa5f4
almamuntmn <almamuncool88@gmail.com>
2025-11-16 19:09:15 +0600

    addedp

--- components/ComingSoon.tsx
+++ components/ComingSoon.tsx
@@ -4,6 +4,7 @@ const ComingSoon = () => {
     return (
         <div>
             <h1 className='text-5xl text-center'>Coming Soon</h1>
+            <p>Please check back later</p>
         </div>
     );
 };
