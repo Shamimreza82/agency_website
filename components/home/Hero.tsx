cd82ffef8eec9e08233344c3f7a989eaa33b0f3a
almamuntmn <almamuncool88@gmail.com>
2025-11-16 21:12:25 +0600

    added hero texts

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,24 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
-    return (
-        <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content flex-col lg:flex-row">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                    className="max-w-sm rounded-lg shadow-2xl"
-                />
-                <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
-                    <button className="btn btn-primary">Get Started</button>
-                </div>
-            </div>
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row w-11/12 mx-auto">
+        <div>
+          <h1 className="text-5xl font-bold text-center">
+            Designing experiences.
+            <br />
+            <span className="text-primary">Developing</span> results.
+          </h1>
+          <p className="py-6 text-center w-lg mx-auto text-accent">
+            Get your desired design services from our talented designers and
+            developers around the word at a respectable price.
+          </p>
+          <div className="flex gap-4">
+            <input
+              className="bg-white border text-gray-900 text-sm rounded-full focus:border-[#02FF95] block w-full px-5"
+              type="email"
+              name="email"
+              id=""
+              placeholder="Your work email"
+            />
+            <button className="btn btn-primary text-secondary rounded-full">
+              Book a call
+            </button>
+          </div>
+          <p className="text-center text-accent mt-5">4.6/5 on 15+ Business, 45+ Happy Clients</p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
