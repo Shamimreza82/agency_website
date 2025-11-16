16183f8c3da4af146db630f156ab93686148d0e4
Shamimreza82 <shamimrezaone@gmail.com>
2025-11-17 01:43:27 +0600

    added node mailera

---
+++ app/(site)/book-a-call/page.tsx
@@ -0,0 +1,12 @@
+import BookingCallForm from '@/components/form/BookingCallForm'
+import React from 'react'
+
+const BookACallPage = () => {
+  return (
+    <div>
+        <BookingCallForm />
+    </div>
+  )
+}
+
+export default BookACallPage
