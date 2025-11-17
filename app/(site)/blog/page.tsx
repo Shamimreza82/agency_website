f63e9f90b46f25b3e5058c65ce35dc3fccd61c17
Shamim Reza <shamimrezaone@gmail.com>
2025-11-17 14:24:55 +0600

    added blog route

---
+++ app/(site)/blog/page.tsx
@@ -0,0 +1,12 @@
+import Blogs from '@/components/pages/Blogs/Blogs'
+import React from 'react'
+
+const BlogsPage = () => {
+  return (
+    <div>
+        <Blogs/>
+    </div>
+  )
+}
+
+export default BlogsPage
